6a366ee8f4c14c38f138ae038dc7e0aea5011e21
BuBuStuff <150358542+BuBuStuff@users.noreply.github.com>
2024-09-02 22:24:07 +0800

    Update loadQuote.js

--- home/loadQuote.js
+++ home/loadQuote.js
@@ -1,17 +1,3 @@
-//Copyright 2024 BuBuStuff
-
-/*Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at*/
-
-//    http://www.apache.org/licenses/LICENSE-2.0
-
-/*Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.*/
-
 let quotes = ["War does not determine who is right - only who is left.",
 	"When you do crazy things, expect crazy results",
 	"I dream of a better tomorrow, where chickens can cross the road and not be quested about their motives",
