7705e18d14ea74ea0cb52000e87b893051830b8e
BuBuStuff <150358542+BuBuStuff@users.noreply.github.com>
2024-09-19 21:42:13 +0800

    Update loadQuote.js

--- home/loadQuote.js
+++ home/loadQuote.js
@@ -160,7 +160,8 @@ let quotes = ["Item in index 0 is not in use.", "War does not determine who is r
 	"The reason I talk to myself is because I’m the only one whose answers I accept.",
 	"I love mankind, it's the people I can't stand.",
 	"It's no use going back to yesterday, because I was a different person then.",
-	"ADHD is weird. You could be getting water and the next thing you know you\'re reading about Greenland, like how I was supposed to be doing my homework when I made this."
+	"ADHD is weird. You could be getting water and the next thing you know you\'re reading about Greenland, like how I was supposed to be doing my homework when I made this.",
+	"A padlock only stops an honest man."
 	];
 
 	console.log("Output active");
