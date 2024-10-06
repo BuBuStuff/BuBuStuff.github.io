8465aab73211a747f650ae59a1f87c62ba7206cd
BuBuStuff <150358542+BuBuStuff@users.noreply.github.com>
2024-10-06 11:45:38 +0800

    Update 1.js

--- test/sets/1.js
+++ test/sets/1.js
@@ -1,81 +1,65 @@
 const questions = [
 {
-  //'selector': 'p.out',
-  'proposal': 'p.out',
+  'proposal': 'Are cupcakes made of cupcake?',
   'answer': 'yes' },
 
 {
-  'selector': 'h2 ~ p',
   'proposal': 'h2 + p',
   'answer': 'no' },
 
 {
-  'selector': 'h2 ~ p',
   'proposal': 'h2 ~ p',
   'answer': 'yes' },
 
 {
-  'selector': '#section1',
   'proposal': '[id="section1"]',
   'answer': 'yes' },
 
 {
-  'selector': 'h2 + p',
   'proposal': 'h2 + p',
   'answer': 'yes' },
 
 {
-  'selector': 'section p:first-of-type',
   'proposal': 'section p:first-child',
   'answer': 'no' },
 
 {
-  'selector': 'section p:first-of-type',
   'proposal': 'section p:first-of-type',
   'answer': 'yes' },
 
 {
-  'selector': 'main > *',
   'proposal': 'main *',
   'answer': 'no' },
 
 {
-  'selector': 'main > *',
   'proposal': 'main > *',
   'answer': 'yes' },
 
 {
-  'selector': 'main p',
   'proposal': 'h2 ~ p',
   'answer': 'no' },
 
 {
-  'selector': 'main > p',
   'proposal': 'main p',
   'answer': 'no' },
 
 {
-  'selector': 'main > p',
   'proposal': 'main > p',
   'answer': 'yes' },
 
 {
-  'selector': 'footer div',
   'proposal': 'footer div',
   'answer': 'yes' },
 
 {
-  'selector': 'footer > div',
   'proposal': 'footer > div',
   'answer': 'yes' },
 
 {
-  'selector': 'footer > :nth-child(n)',
   'proposal': 'footer > :nth-child(n)',
   'answer': 'yes' },
 
 {
-  'selector': 'footer > div',
   'proposal': 'footer div',
   'answer': 'no' },
 
